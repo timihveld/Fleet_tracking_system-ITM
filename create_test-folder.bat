60a71efac1e589e2b196b55bc9f8cc8406b9bf80
timihveld <97671493+timihveld@users.noreply.github.com>
2025-01-17 21:15:14 +0100

    Update create_test-folder.bat

--- create_test-folder.bat
+++ create_test-folder.bat
@@ -1,48 +1,41 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-:: Set the path for the installation folder
-set INSTALL_DIR=C:\firefox_installer
+:: Path to save the installer
+set INSTALLER_PATH=C:\google_chrome\firefox_installer.exe
 
-:: Check if the folder already exists and create it if necessary
-if not exist "%INSTALL_DIR%" (
-    mkdir "%INSTALL_DIR%"
+:: URL for Firefox 64-bit version
+set FIREFOX_URL=https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US
+
+:: Check if Firefox is already installed
+if exist "C:\Program Files\Mozilla Firefox\firefox.exe" (
+    echo Firefox is already installed.
+    exit /b
 )
 
-:: Set the correct URL for the Firefox installer (ESR version 115.11.0)
-set FIREFOX_URL=https://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/en-US/Firefox%20Setup%20115.11.0esr.msi
-
-:: Change to the installation folder
-cd /d "%INSTALL_DIR%"
-
-:: Download Firefox installer
+:: Download Firefox if not already installed
 echo Downloading Firefox...
-powershell -Command "Invoke-WebRequest -Uri %FIREFOX_URL% -OutFile %INSTALL_DIR%\firefox_installer.msi"
+powershell -Command "Invoke-WebRequest -Uri !FIREFOX_URL! -OutFile !INSTALLER_PATH!"
 
-:: Check if the file was downloaded successfully
-if exist "%INSTALL_DIR%\firefox_installer.msi" (
-    echo Firefox installer downloaded successfully.
+:: Check if the download was successful
+if exist "!INSTALLER_PATH!" (
+    echo Firefox downloaded successfully.
+    echo Installing Firefox...
+
+    :: Run the installer silently
+    start /wait !INSTALLER_PATH! /silent
+
+    :: Check if the installation was successful
+    if exist "C:\Program Files\Mozilla Firefox\firefox.exe" (
+        echo Firefox has been installed successfully.
+    ) else (
+        echo Firefox installation failed.
+        exit /b 1
+    )
 ) else (
-    echo Failed to download Firefox installer.
+    echo Failed to download Firefox.
     exit /b 1
 )
 
-:: Run the installer silently
-echo Installing Firefox...
-msiexec /i "%INSTALL_DIR%\firefox_installer.msi" /quiet /norestart
-
-:: Check if installation was successful
-if exist "C:\Program Files\Mozilla Firefox\firefox.exe" (
-    echo Firefox installed successfully.
-    echo Firefox has been successfully installed! >> C:\installation_log.txt
-) else (
-    echo Installation failed.
-    echo Firefox installation failed! >> C:\installation_log.txt
-)
-
 :: Clean up installer
-del "%INSTALL_DIR%\firefox_installer.msi"
-
-:: End of script
-echo Task completed. Press any key to exit.
-pause
+del /f /q "!INSTALLER_PATH!"
