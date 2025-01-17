9188834d980c514a7894d4ceee1450f394595e04
timihveld <97671493+timihveld@users.noreply.github.com>
2025-01-17 20:52:16 +0100

    Update create_test-folder.bat

--- create_test-folder.bat
+++ create_test-folder.bat
@@ -1,1 +1,12 @@
-mkdir C:\tbm
+@echo off
+:: Maak de map C:\tbm aan als deze nog niet bestaat
+if not exist C:\tbm (
+    mkdir C:\tbm
+)
+
+:: Maak een tekstbestand met de gewenste boodschap
+echo Dit is voor de opdracht van Technische Beheer en Monitoring (TBM) en CI/CD en werkt! > C:\tbm\opdracht_uitgevoerd.txt
+
+:: Bevestiging
+echo Map en bestand zijn succesvol aangemaakt.
+pause
