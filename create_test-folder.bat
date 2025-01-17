d5090876c77bcab536d06433d9839ae04ab2af95
timihveld <97671493+timihveld@users.noreply.github.com>
2025-01-17 20:55:10 +0100

    Update create_test-folder.bat

--- create_test-folder.bat
+++ create_test-folder.bat
@@ -4,9 +4,22 @@ if not exist C:\tbm (
     mkdir C:\tbm
 )
 
-:: Maak een tekstbestand met de gewenste boodschap
+:: Maak een map voor Google Chrome
+if not exist C:\tbm\google_chrome (
+    mkdir C:\tbm\google_chrome
+)
+
+:: Download Google Chrome
+echo Downloaden van Google Chrome...
+curl -o C:\tbm\google_chrome\chrome_installer.exe https://dl.google.com/chrome/install/latest/chrome_installer.exe
+
+:: Installeer Google Chrome
+echo Installeren van Google Chrome...
+C:\tbm\google_chrome\chrome_installer.exe /silent /install
+
+:: Maak een tekstbestand met de boodschap voor TBM en CI/CD
 echo Dit is voor de opdracht van Technische Beheer en Monitoring (TBM) en CI/CD en werkt! > C:\tbm\opdracht_uitgevoerd.txt
 
 :: Bevestiging
-echo Map en bestand zijn succesvol aangemaakt.
+echo Map, bestand, en Google Chrome zijn succesvol aangemaakt en geïnstalleerd.
 pause
